f3e952e46ebf883f2dcb9a4e32490eb07f4f9014
dani <daniel@flink.ai>
2024-03-26 17:36:53 +0800

    fixing bug

--- tests/simple_logon.py
+++ tests/simple_logon.py
@@ -17,6 +17,7 @@ class Strategy:
 
     def run(self):
         done = False
+        print(f"Starting strategy...")
         while not done:
             msg = self.message_queue.get()
             print(f"Message received: {msg}")
@@ -29,10 +30,12 @@ class Strategy:
 
     def start(self):
         self.thread = threading.Thread(target=self.run, args=())
+        self.thread.start()
 
 
 if __name__ == "__main__":
     path_base = PathBase()
+    temp_dir = path_base.temp
     logger = setup_logger("fix_service")
     message_queue = queue.Queue()
     fix_configs = FixSessionConfig(
@@ -47,14 +50,20 @@ if __name__ == "__main__":
     )
     fix_settings = fix_configs.get_fix_session_settings()
 
-    fix_app = App(message_queue, fix_settings, logger, path_base.temp)
+    fix_app = App(message_queue, fix_settings, logger, temp_dir)
     store_factory = fix.FileStoreFactory(fix_settings)
     log_factory = fix.FileLogFactory(fix_settings)
     initiator = fix.SocketInitiator(fix_app, store_factory, fix_settings, log_factory)
 
-    strategy = Strategy(message_queue, lambda _: initiator.stop())
+    strategy = Strategy(message_queue, lambda: initiator.stop())
 
     strategy.start()
     initiator.start()
+
+    strategy.thread.join()
+
+    print(f"Strategy completed")
+
+
 
 
