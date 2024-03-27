5aa3af1730e6a1a40f061ab126040b0857ec4ea5
dani <daniel@flink.ai>
2024-03-27 13:27:01 +0800

    reorg

---
+++ ___setup.py
@@ -0,0 +1,19 @@
+# from setuptools import setup
+#
+# setup(name='phx-fix-base',
+#       version='1.0',
+#       description='Phoenix Prime FIX base functionality',
+#       author='Matrixport',
+#       author_email='daniel.egloff@matrixport.com',
+#       url='https://github.com/mtxpt/phx-fix-base',
+#       packages=['phx',
+#                 'phx.fix',
+#                 'phx.fix.app',
+#                 'phx.fix.model',
+#                 'phx.fix.specs',
+#                 'phx.fix.tracker',
+#                 'phx.fix.utils',
+#                 'phx.utils'],
+#       package_data={'': ['phx/fix/specs/FIX44.xml']},
+#       include_package_data=True,
+#       )
