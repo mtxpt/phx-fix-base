d0a58807770c4cbed1d447f6fb40476f71c710dd
dani <daniel@flink.ai>
2024-04-03 13:19:41 +0800

    updating logger creation

--- src/phx/utils/logger.py
+++ src/phx/utils/logger.py
@@ -2,27 +2,23 @@ import logging
 
 
 def setup_logger(logger_name, level=logging.INFO) -> logging.Logger:
-    lz = logging.getLogger(logger_name)
+    logger = logging.getLogger(logger_name)
     formatter = logging.Formatter('%(asctime)s : %(message)s')
-    lz.setLevel(level)
-    # Log to std.err
+    logger.setLevel(level)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
-    lz.addHandler(stream_handler)
+    logger.addHandler(stream_handler)
     # Multiprocess logging:
     # https://docs.python.org/3/howto/logging-cookbook.html#logging-to-a-single-file-from-multiple-processes
-    return lz
+    return logger
 
 
-def set_file_loging_handler(logger_or_name, log_file):
+def set_file_loging_handler(logger_or_name, log_file) -> logging.Logger:
     if isinstance(logger_or_name, str):
-        lz = logging.getLogger(logger_or_name)
+        logger = logging.getLogger(logger_or_name)
     else:
-        lz = logger_or_name
-    formatter = logging.Formatter('%(asctime)s : %(message)s')
+        logger = logger_or_name
     file_handler = logging.FileHandler(log_file, mode='w')
-    file_handler.setFormatter(formatter)
-    lz.addHandler(file_handler)
-
-
-# phx_log = setup_logger('phx_log')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
+    logger.addHandler(file_handler)
+    return logger
