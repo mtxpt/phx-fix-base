daa680a292ec7c3b8e7d967a77598d0e7f470666
dani <daniel@flink.ai>
2024-04-04 00:31:10 +0800

    fixing simple_logon.py so that it works also without any connection - better login and clean up

--- tests/simple_logon.py
+++ tests/simple_logon.py
@@ -1,11 +1,11 @@
 import queue
+import time
 import threading
 import logging
-from typing import Callable
 from pathlib import Path
 
 from phx.fix.app import App, AppRunner, FixSessionConfig
-from phx.fix.model import Logon, Create
+from phx.fix.model import Create, Logon, Logout
 from phx.utils import setup_logger, set_file_loging_handler, make_dirs
 
 
@@ -16,37 +16,62 @@ def temp_dir() -> Path:
 
 class Strategy:
 
-    def __init__(self, message_queue: queue.Queue, stop: Callable):
+    def __init__(self, message_queue: queue.Queue, runner: AppRunner):
         self.message_queue = message_queue
+        self.logged_on = False
         self.thread = None
-        self.stop = stop
+        self.runner = runner
+        self.logger = self.runner.logger
+        self.delay = 1
 
     def run(self):
-        done = False
-        print(f"Starting strategy...")
-        while not done:
+        error = "no errors"
+        while True:
             msg = self.message_queue.get()
-            print(f"Message received: {msg}")
+            self.logger.info(f"****> {msg} received")
             if isinstance(msg, Create):
-                print(f"Create: {msg}")
+                self.logger.info(f"****> {msg}")
             elif isinstance(msg, Logon):
-                print(f"Logon: {msg}")
-                done = True
-                self.stop()
+                self.logger.info(f"****> {msg} - going to stop in {self.delay}s")
+                time.sleep(2)
+                self.logged_on = True
+                self.logger.info(f"****> Stopping FIX app...")
+                self.runner.stop()
+            elif isinstance(msg, Logout) and not self.logged_on:
+                self.logger.info(f"****> {msg} without Logon first - most likely connection problem - stopping now")
+                error = "connection problems"
+                break
+            elif isinstance(msg, Logout) and self.logged_on:
+                break
+            else:
+                self.logger.info(f"****> Unexpected message {msg}")
+
+        self.logger.info(f"****> Message loop completed with {error}")
 
     def start(self):
+        self.logger.info(f"====> Starting FIX app...")
+        self.runner.start()
+        self.logger.info(f"Starting strategy processing FIX messages...")
         self.thread = threading.Thread(target=self.run, args=())
         self.thread.start()
+
+    def stop(self):
+        self.thread.join()
+        self.logger.info(f"<==== Strategy completed.")
 
 
 if __name__ == "__main__":
     export_dir = temp_dir() / "simple_logon"
     make_dirs(export_dir)
+
+    # setup logger to console and log file
     logger = set_file_loging_handler(
         setup_logger("fix_service", level=logging.INFO),
         export_dir / "fix_service.log"
     )
     message_queue = queue.Queue()
+
+    # FIX session config
     fix_configs = FixSessionConfig(
         sender_comp_id="test",
         target_comp_id="proxy",
@@ -60,13 +85,8 @@ if __name__ == "__main__":
     )
     fix_session_settings = fix_configs.get_fix_session_settings()
 
-    app = App(message_queue, fix_session_settings, logger, export_dir)
+    app = App(message_queue, fix_session_settings, logger, str(export_dir))
     app_runner = AppRunner(app, fix_session_settings, fix_configs.get_session_id(), logger)
-    strategy = Strategy(message_queue, lambda: app_runner.stop())
-
-    app_runner.start()
+    strategy = Strategy(message_queue, app_runner)
     strategy.start()
-
-    strategy.thread.join()
-
-    print(f"Strategy completed")
+    strategy.stop()
